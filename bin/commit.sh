9d068d618ad02e867d573e607b474611959402e3
qianchen <qianchen245@gmail.com>
2021-09-06 17:08:14 +0800

    diagram

--- bin/commit.sh
+++ bin/commit.sh
@@ -1,3 +1,5 @@
+npm run build
 git add *
+git add dist -f
 git commit -am $1
 git push
---
+++ src/store/config.js
@@ -0,0 +1,3 @@
+export const API_HOST = window.location.host == 'diagram.zuker.im'
+    ? 'http://spider.zuker.im'
+    : 'http://local.api.spider.com'
