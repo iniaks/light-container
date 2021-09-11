86b2118db7eccbb958bd575c658e04e15b09f195
qianchen <qianchen245@gmail.com>
2021-09-11 10:36:00 +0800

    host

--- src/views/Combination/index.vue
+++ src/views/Combination/index.vue
@@ -203,7 +203,7 @@
                     lines,
                     translations: translations
                 }
-                axios.post('http://local.api.spider.com/diagram/combination', data).then(() => {
+                axios.post(`${API_HOST}/diagram/combination`, data).then(() => {
                     // that.update()
                     that.$router.push({name: 'home'})
                 }).catch(err => {
